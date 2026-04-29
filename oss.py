a547805521fb5cc33e4b6bfab14ad6272769fa2b
Jinseok-Lee <jinsoek94@gmail.com>
2026-04-29 10:40:53 +0900

    feat: 함수화 complete

--- oss.py
+++ oss.py
@@ -1,26 +1,26 @@
 def add(num1, num2):
-    print(num1 + num2)
+    return(num1 + num2)
 
 def sub(num1, num2):
-    print(num1 - num2)
+    return(num1 - num2)
 
 def mul(num1, num2):
-    print(num1 * num2)
+    return(num1 * num2)
 
 def div(num1, num2):
-    print(num1 / num2)
+    return(num1 / num2)
 
 fir_num, cal, sec_num = input("cal:   ").split()
 a = int(fir_num)
 b = int(sec_num)
 if cal == "+":
-    add(a, b)
+    print(add(a, b))
 elif cal == "-":
-    sub(a, b)
+    print(sub(a, b))
 elif cal == "*":
-    mul(a, b)
+    print(mul(a, b))
 else:
     if b == 0:
         print("Error: zero division")
     else:
-        div(a, b)
+        print(div(a, b))
