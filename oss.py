8b3c334bfb04f25cb6efe3424dd6757369c52d5d
Jinseok-Lee <jinsoek94@gmail.com>
2026-04-29 10:24:57 +0900

    add: 사칙연산

---
+++ oss.py
@@ -0,0 +1,14 @@
+fir_num, cal, sec_num = input().split()
+a = int(fir_num)
+b = int(sec_num)
+if cal == "+":
+    print(a+b)
+elif cal == "-":
+    print(a - b)
+elif cal == "*":
+    print(a * b)
+else:
+    if b == 0:
+        print("Error: zero division")
+    else:
+        print(a / b)
